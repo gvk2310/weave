78382def4946134e0b928d16223577c1c3c6605c
Sen <soumya.a.sen@accenture.com>
2020-04-24 12:00:14 +0530

    :

---
+++ Authentication/src/config/config.py
@@ -0,0 +1,7 @@
+from .. import app
+
+app.config['MONGODB_DB'] = 'devnetopsV2'
+app.config['MONGODB_HOST'] = 'localhost'
+app.config['MONGODB_PORT'] = 27017
+app.config['SECRET_KEY'] = 'b6622e14a72744af5d991e5c6e262e9ba2f76c'
+app.config['SECURITY_PASSWORD_SALT'] = '4af5d991e5c6e262e9ba2f76c'
