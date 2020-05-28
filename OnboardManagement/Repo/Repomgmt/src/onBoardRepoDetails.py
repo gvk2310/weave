e60811ced6c383020ff0e68c944b08b01c06dcdd
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-05-28 12:07:49 +0000

    onBoardRepoDetails.py edited online with Bitbucket

--- OnboardManagement/Repo/Repomgmt/src/onBoardRepoDetails.py
+++ OnboardManagement/Repo/Repomgmt/src/onBoardRepoDetails.py
@@ -56,33 +56,28 @@ class OnBoardRepoDetails(Resource):
                  e)
             
     
-    def addDataToVault(repo_name, repodata):
-      	resp = requests.post(
-						f"{vault_url}/v1/secret/data/{repo_name}", headers={'X-Vault-Token': vault_token, 'Content-Type': 'application/json'}, data=json.dumps(repodata))
-            if resp.status_code == 200:
-				return {'msg': 'Data added successfully'}, resp.status_code
-			else:
-				return {'msg': 'Data could not be added'}, 400
-              
-       
     @verifyToken
     def post(self, repo_name):
         try:
-           	repo_username = request.json['username']
-			repo_password = request.json['password']
-			repo_url = request.json['url']
-			repo_type = request.json['repotype']
-			repodata = {"data": request.json}
-			if repo_type == 'Jfrog':
-				validate_credentials = requests.get(f"{repo_url}/artifactory/api/system/", auth=(repo_username, repo_password))
+            repo_username = request.json['username']
+            repo_password = request.json['password']
+            repo_url = request.json['url']
+            repo_type = request.json['repotype']
+            repodata = {"data": request.json}
+            if repo_type == 'Jfrog':
+                validate_credentials = requests.get(f"{repo_url}/artifactory/api/system/", auth=(repo_username, repo_password))
             elif repo_type == 'Nexus':
-              	validate_credentials = requests.get(f"{repo_url}/service/local/status", auth=(repo_username, repo_password))
-			if validate_credentials.status_code == 200:
-            	addDataToVault(repo_name, repodata)
-			else:
-				return {'msg': 'Repository details are invalid'}, 400
+                validate_credentials = requests.get(f"{repo_url}/service/local/status", auth=(repo_username, repo_password))
+            if validate_credentials.status_code == 200:
+                resp = requests.post(f"{vault_url}/v1/secret/data/{repo_name}", headers={'X-Vault-Token': vault_token}, data=json.dumps(repodata))
+                if resp.status_code == 200:
+                    return {'msg': 'Data added successfully'}, resp.status_code
+                else:
+                    return {'msg': 'Data could not be added'}, 400
+            else:
+                return {'msg': 'Repository details are invalid'}, 400
         except Exception as e:
-            logs('Unable to post data to vault', traceback.format_exc(), e)
+            logs('Data could not be added to Vault', traceback.format_exc(), e)
             
             
     @verifyToken
@@ -91,9 +86,6 @@ class OnBoardRepoDetails(Resource):
             resp = requests.delete(
                 f"{vault_url}/v1/secret/data/{repo_name}",
                 headers={'X-Vault-Token': vault_token})
-            if resp.status_code == 204:
-                return {'msg': 'Data deleted successfully'}, resp.status_code
-            else:
-                return {'msg': 'Data deletion failed'}, 404
+            return resp.json(), resp.status_code, resp.headers
         except Exception as e:
           	logs('Unable to delete data from vault', traceback.format_exc(), e)
