7d7a25a63b5ccaf4747e58650b27f560de6772b1
soumya.a.sen <soumya.a.sen>
2020-10-06 11:29:22 +0530

    putting back sse components

---
+++ OnboardManagement/Onboarding/src/lib/sse.py
@@ -0,0 +1,52 @@
+import os
+import time
+import json
+import redis
+import datetime
+import traceback
+from ..log import logger
+
+redis_url = os.environ['redis_url']
+redis_host = redis_url.split(':')[0]
+redis_port = redis_url.split(':')[1]
+
+
+class SSEGenerator:
+    def __init__(self):
+        self.closed = False
+        self.expiry = datetime.datetime.now() + datetime.timedelta(minutes=30)
+
+    def close(self):
+        self.closed = True
+
+    def __iter__(self):
+        event = 'assets'
+        client = redis.Redis(host=redis_host, port=redis_port)
+        p = client.pubsub()
+        p.subscribe(event)
+        while not self.closed and datetime.datetime.now() < self.expiry:
+            resp = p.get_message()
+            if resp and not resp['data'] == 1:
+                msg = resp['data'].decode('utf-8')
+                payload = self.format_sse(data=msg, event=event)
+                yield payload
+            else:
+                time.sleep(1)
+
+    @staticmethod
+    def format_sse(data, event):
+        msg = f'data: {data}\n\n'
+        if event:
+            msg = f'event: {event}\n{msg}'
+        return msg
+
+
+def publish_asset_events(**kwargs):
+    try:
+        client = redis.Redis(host=redis_host, port=redis_port)
+        client.publish('assets', json.dumps(kwargs))
+        return True
+    except Exception as e:
+        logger.error("Unable to publish message in redis")
+        logger.debug(traceback.format_exc())
+        logger.error(e)
