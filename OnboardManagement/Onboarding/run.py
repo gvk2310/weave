c7f3e84e49c1fbf1a42468b49397e65c42cec1a5
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-10-28 14:22:13 +0000

    run.py edited online with Bitbucket

--- OnboardManagement/Onboarding/run.py
+++ OnboardManagement/Onboarding/run.py
@@ -1,5 +1,5 @@
 from src import app
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8080)
