dce73c63fda7a88301efa9f92b6a0ac57ad0fed5
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-10-29 08:12:02 +0000

    setup.py edited online with Bitbucket

--- OnboardManagement/setup.py
+++ OnboardManagement/setup.py
@@ -5,6 +5,13 @@ ext_modules = [
         Extension("Onboarding.src.api.resources",  ["Onboarding/src/api/resources.py"]),
                 Extension("Onboarding.src.db.db",  ["Onboarding/src/db/db.py"]),
                 Extension("Onboarding.src.config.config",  ["Onboarding/src/config/config.py"]),
+  				Extension("Onboarding.src.lib.cloud_validate",  ["Onboarding/src/lib/cloud_validate.py"]),
+  				Extension("Onboarding.src.lib.commonfunctions",  ["Onboarding/src/lib/commonfunctions.py"]),
+  				Extension("Onboarding.src.lib.jfrog",  ["Onboarding/src/lib/jfrog.py"]),
+  				Extension("Onboarding.src.lib.nexus",  ["Onboarding/src/lib/nexus.py"]),
+  				Extension("Onboarding.src.lib.sse",  ["Onboarding/src/lib/sse.py"]),
+  				Extension("Onboarding.src.lib.vault",  ["Onboarding/src/lib/vault.py"]),
+  				Extension("Onboarding.src.__init__",  ["Onboarding/src/__init__.py"]),
                 Extension("Onboarding.src.log",  ["Onboarding/src/log.py"])
 ]
 setup(
