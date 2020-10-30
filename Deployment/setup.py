1b591d35785f7b0d518de18fba4ba00e10774d90
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-10-30 10:32:25 +0000

    setup.py edited online with Bitbucket

--- Deployment/setup.py
+++ Deployment/setup.py
@@ -2,10 +2,14 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 ext_modules = [
-        Extension("Onboarding.src.api.resources",  ["Onboarding/src/api/resources.py"]),
-                Extension("Onboarding.src.db.db",  ["Onboarding/src/db/db.py"]),
-                Extension("Onboarding.src.config.config",  ["Onboarding/src/config/config.py"]),
-                Extension("Onboarding.src.log",  ["Onboarding/src/log.py"])
+        Extension("Deploy.src.api.resources",  ["Deploy/src/api/resources.py"]),
+                Extension("Deploy.src.db.db",  ["Deploy/src/db/db.py"]),
+                Extension("Deploy.src.config.config",  ["Deploy/src/config/config.py"]),
+  				Extension("Deploy.src.lib.awsFunctions",  ["Deploy/src/lib/awsFunctions.py"]),
+  				Extension("Deploy.src.lib.commonfunctions",  ["Deploy/src/lib/commonfunctions.py"]),
+  				Extension("Deploy.src.lib.sse",  ["Deploy/src/lib/sse.py"]),
+  				Extension("Deploy.src.__init__",  ["Deploy/src/__init__.py"]),
+                Extension("Deploy.src.log",  ["Deploy/src/log.py"])
 ]
 setup(
         name = 'devnetops',
