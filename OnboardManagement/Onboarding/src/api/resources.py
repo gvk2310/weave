9bb4c1a4ead7f642241646b6e4f6a8328c479255
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-11-11 03:40:42 +0000

    resources.py edited online with Bitbucket

--- OnboardManagement/Onboarding/src/api/resources.py
+++ OnboardManagement/Onboarding/src/api/resources.py
@@ -211,7 +211,7 @@ class Repository(Resource):
                             required=True)
         parser.add_argument('repo_vendor', nullable=False,
                             type=non_empty_string, required=True,
-                            choices=('nexus', 'jfrog'))
+                            choices=['nexus', 'jfrog'])
         parser.add_argument('repo_url', type=inputs.url, required=True)
         parser.add_argument('repo_username', nullable=False,
                             type=non_empty_string, required=True)
