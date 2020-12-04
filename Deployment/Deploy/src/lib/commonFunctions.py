b31c1f9e8854f25881a0b99995cfe64deb7e910d
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-12-04 21:59:41 +0000

    commonFunctions.py edited online with Bitbucket

--- Deployment/Deploy/src/lib/commonFunctions.py
+++ Deployment/Deploy/src/lib/commonFunctions.py
@@ -18,7 +18,7 @@ jenkins_token = os.environ['jenkins_token']
 onboarding_url = os.environ['onboarding_url']
 
 
-#token_auth_url = os.environ['usermgmtUrl']
+token_auth_url = os.environ['usermgmtUrl']
 
 
 def nonEmptyString(value):
@@ -65,24 +65,24 @@ def verifyJenkins(fn):
     return wrapper
 
 
-#def verifyToken(fn):
-#     @wraps(fn)
-#     def wrapper(*args, **kwargs):
-#         if 'Authorization' not in request.headers.keys() or \
-#                 request.headers['Authorization'].split()[0] != 'Bearer':
-#             return {"msg": "Token required"}, 500
-#         token = request.headers['Authorization'].split()[1]
-#         perm = 'read' if request.method == 'GET' else 'write'
-#         resp = requests.get(
-#             f"{token_auth_url}/isauthorized/vault/{perm}",
-#             headers={f'Authorization': f'Bearer {token}',
-#                      'Content-Type': 'application/json'},
-#         )
-#         if resp.status_code != 200:
-#             return resp.json(), resp.status_code
-#         return fn(*args, **kwargs)
+def verifyToken(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if 'Authorization' not in request.headers.keys() or \
+                request.headers['Authorization'].split()[0] != 'Bearer':
+            return {"msg": "Token required"}, 500
+        token = request.headers['Authorization'].split()[1]
+        perm = 'read' if request.method == 'GET' else 'write'
+        resp = requests.get(
+            f"{token_auth_url}/isauthorized/vault/{perm}",
+            headers={f'Authorization': f'Bearer {token}',
+                     'Content-Type': 'application/json'},
+        )
+        if resp.status_code != 200:
+            return resp.json(), resp.status_code
+        return fn(*args, **kwargs)
 
-#     return wrapper
+    return wrapper
 
 
 def assetDownloadDetails(assets):
