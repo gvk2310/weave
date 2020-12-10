3976855e41639c0fc7b4d96aa3dd57ef4584c371
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-12-10 14:24:26 +0000

    resources.py edited online with Bitbucket

--- UserManagement/UserMgmt/src/api/resources.py
+++ UserManagement/UserMgmt/src/api/resources.py
@@ -1,7 +1,6 @@
 import datetime
 from ..db import db
-from ..lib.commonFunctions import admin_required, nonEmptyString, \
-    nonEmptyEmail, nonEmptyPasswString, formatList
+from ..lib.commonFunctions import *
 from flask_restful import Resource, reqparse, inputs
 from flask_jwt_extended import create_access_token, get_jwt_identity, \
     jwt_required
