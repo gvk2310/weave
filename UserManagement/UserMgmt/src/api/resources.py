88a8f71691929f49b29130583fce4ca769be6884
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2020-12-22 12:21:07 +0000

    resources.py edited online with Bitbucket

--- UserManagement/UserMgmt/src/api/resources.py
+++ UserManagement/UserMgmt/src/api/resources.py
@@ -226,12 +226,12 @@ class Role(Resource):
         if not isinstance(services, list):
             return {'message': {'services': services}}, 400
         if args['action'] == 1:
-            if db.addSvcToRole(args['role'], services) == 1:
-                return {'message': 'One of the services not found'}, 400
-            elif db.addSvcToRole(args['role'], services) == 2:
-                return {'message': 'Service already exist for this role'}, 400            
+            if db.addSvcToRole(args['role'], services) == 2:
+                return {'message': 'Service(s) added to role'}, 200
             elif db.addSvcToRole(args['role'], services) == 3:
-                return {'message': 'Service is added to the role'}, 200            
+                return {'message': 'Service(s) doesnt exist'}, 400            
+            elif db.addSvcToRole(args['role'], services) == 1:
+                return {'message': 'Service(s) already associated with the role'}, 400                 
             
         elif args['action'] == 2:
             if db.remSvcFrmRole(args['role'], services) == 2:
