9cb16b47db01d4834794b8507381b76ebcf4b569
Rajanna, Megha A <megha.a.rajanna@accenture.com>
2021-01-12 12:22:22 +0000

    sse.py edited online with Bitbucket

--- OnboardManagement/Onboarding/src/lib/sse.py
+++ OnboardManagement/Onboarding/src/lib/sse.py
@@ -11,38 +11,6 @@ redis_host = redis_url.split(':')[0]
 redis_port = redis_url.split(':')[1]
 
 
-#class SSEGenerator:
-#    def __init__(self, event):
-#        #self.event = broadcast_type
-#        self.event = event
-#        self.closed = False
-#        self.expiry = datetime.datetime.now() + datetime.timedelta(minutes=5)
-
-#    def close(self):
-#        self.closed = True
-
-#    def __iter__(self):
-#        #event = 'onboard'
-#        client = redis.Redis(host=redis_host, port=redis_port)
-#        p = client.pubsub()
-#        p.subscribe(self.event)
-#        while not self.closed and datetime.datetime.now() < self.expiry:
-#            resp = p.get_message()
-#            if resp and not resp['data'] == 1:
-#                msg = resp['data'].decode('utf-8')
-#                payload = self.format_sse(data=msg, event=self.event)
-#                yield payload
-#            else:
-#                time.sleep(1)
-
-#    @staticmethod
-#    def format_sse(data, event):
-#        msg = f'data: {data}\n\n'
-#        if type:
-#            msg = f'event: {event}\n{msg}'
-#        return msg
-
-
 def publish_onboard_events(event, data):
     try:
         client = redis.Redis(host=redis_host, port=redis_port)
