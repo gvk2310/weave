0e85be5322d8056412e421a99d2da7e80613707b
Rajanna, Megha A <megha.a.rajanna@accenture.com>
2021-01-13 05:41:54 +0000

    config.py edited online with Bitbucket

--- SSE/sse/src/config/config.py
+++ SSE/sse/src/config/config.py
@@ -1,10 +1,5 @@
 import os
 from .. import app
 
-#app.config['MONGODB_SETTINGS'] = {
-#    'host': os.environ['mongohost'],
-#    'connect': False
-#}
 app.config['upload_folder'] = 'tmp'
-os.environ['onboardUrl'] = 'http://onboard-test.ethan.svc.cluster.local:80'
-os.environ['deploymentUrl'] = 'http://deployment.ethan.svc.cluster.local:80'
+
