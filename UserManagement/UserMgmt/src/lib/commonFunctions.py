544a247d9a8e0c9816ee7ddfce8f2431c8b7dc7b
Rajanna, Megha A <megha.a.rajanna@accenture.com>
2021-02-17 16:36:59 +0000

    commonFunctions.py edited online with Bitbucket

--- UserManagement/UserMgmt/src/lib/commonFunctions.py
+++ UserManagement/UserMgmt/src/lib/commonFunctions.py
@@ -3,6 +3,7 @@ from ..db import db
 from flask_jwt_extended import get_jwt_identity
 from functools import wraps
 from kubernetes import client, config
+from ..log import logger
 
 
 def admin_required(fn):
@@ -61,18 +62,23 @@ def returnNotMatches(service_list, actual_list):
     return [x for x in service_list if x not in actual_list]
   
 def endpoints():
-  end_points = []
-  config.load_incluster_config()
-  v1= client.CoreV1Api()
-  ret = v1.list_config_map_for_all_namespaces(watch=False)
-  for i in ret.items:
-    if(i.metadata.name=="nginx-conf"):
-      lines=i.data
-      str_lines = str(lines)
-      lines=str_lines.split()
-      for line in lines:
-        split_devnetops=re.findall("devnetops",line)
-        if split_devnetops == ['devnetops']:
-          end_point=line
-          end_points.append(end_point)
-  return end_points
+    try:
+        end_points = []
+        config.load_incluster_config()
+        v1 = client.CoreV1Api()
+        ret = v1.list_config_map_for_all_namespaces(watch=False)
+        for i in ret.items:
+            if (i.metadata.name == "nginx-conf"):
+                lines = i.data
+                str_lines = str(lines)
+                lines = str_lines.split()
+                for line in lines:
+                    split_devnetops = re.findall("devnetops", line)
+                    if split_devnetops == ['devnetops']:
+                        end_point = line
+                        end_points.append(end_point)
+        return end_points
+    except Exception as e:
+        logger.error('Unable to get endpoints from kubernetes')
+        logger.error(e)
+
