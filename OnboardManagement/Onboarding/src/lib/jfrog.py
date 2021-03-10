31c8d56568268b1019c8f1029769550195eb1b77
Chandrashekar, Ranjitha <r.f.chandrashekar@accenture.com>
2021-03-10 08:33:36 +0000

    jfrog.py edited online with Bitbucket

--- OnboardManagement/Onboarding/src/lib/jfrog.py
+++ OnboardManagement/Onboarding/src/lib/jfrog.py
@@ -6,9 +6,9 @@ from ..log import logger
 def validateJfrog(args):
     try:
         jfrog_url = args['repo_url'][:-1]
-        jfrog_url = args['repo_url'].split('artifactory')[0]
+        jfrog_url = args['repo_url'].split('/artifactory')[0]
         repo_details = requests.get(
-            f"{jfrog_url}artifactory/api/repositories",
+            f"{jfrog_url}/artifactory/api/repositories",
             auth=(args['repo_username'], args['repo_password']))
         if repo_details.status_code != 200:
             raise Exception(
