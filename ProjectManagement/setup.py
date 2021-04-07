7033235c3c06c1a6ca81e5d61fe69f60760c2e34
Chandrashekar, Ranjitha <r.f.chandrashekar@accenture.com>
2021-04-07 13:44:27 +0000

    setup.py edited online with Bitbucket

--- ProjectManagement/setup.py
+++ ProjectManagement/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 ext_modules = [
-        Extension("AccessMgmt.src.api.resources",  ["AccessMgmt/src/api/resources.py"]),
-                Extension("AccessMgmt.src.db.db",  ["AccessMgmt/src/db/db.py"]),
-                Extension("AccessMgmt.src.config.config",  ["AccessMgmt/src/config/config.py"]),
-  				Extension("AccessMgmt.src.lib.commonFunctions",  ["AccessMgmt/src/lib/commonFunctions.py"]),
-  				Extension("AccessMgmt.src.__init__",  ["AccessMgmt/src/__init__.py"]),
-  				Extension("AccessMgmt.src.log",  ["AccessMgmt/src/log.py"])
+        Extension("ProjectMgmt.src.api.resources",  ["ProjectMgmt/src/api/resources.py"]),
+                Extension("ProjectMgmt.src.db.db",  ["ProjectMgmt/src/db/db.py"]),
+                Extension("ProjectMgmt.src.config.config",  ["ProjectMgmt/src/config/config.py"]),
+  				Extension("ProjectMgmt.src.lib.commonFunctions",  ["ProjectMgmt/src/lib/commonFunctions.py"]),
+  				Extension("ProjectMgmt.src.__init__",  ["ProjectMgmt/src/__init__.py"]),
+  				Extension("ProjectMgmt.src.log",  ["ProjectMgmt/src/log.py"])
 ]
 setup(
         name = 'devnetops',
