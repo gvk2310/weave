3653e8150735b8ae622dd3cd9dfd4a95099636a4
Rajanna, Megha A <megha.a.rajanna@accenture.com>
2021-04-16 16:01:34 +0000

    resources.py edited online with Bitbucket

--- ProjectManagement/ProjectMgmt/src/api/resources.py
+++ ProjectManagement/ProjectMgmt/src/api/resources.py
@@ -17,18 +17,18 @@ class Project(Resource):
 
     def post(self):
         parser = reqparse.RequestParser(trim=True, bundle_errors=True)
-        parser.add_argument('name', type=nonEmptyString, required=True,
+        parser.add_argument('project_name', type=nonEmptyString, required=True,
                             nullable=False)
-        parser.add_argument('details', type=str, required=True, nullable=False)
+        parser.add_argument('project_details', type=str, required=True, nullable=False)
         args = parser.parse_args()
-        check = db.getProject(name=args['name'])
+        check = db.getProject(project_name=args['project_name'])
         if check is not False:
-          if check['name'] == args['name']:
+          if check['project_name'] == args['project_name']:
             return {'message': 'Project already exist'}, 400
         args['project_id'] = datetime.datetime.now().strftime("PR%Y%m%d%H%M%S")
         resp = db.createProject(project_id=args['project_id'],
-                                name=args['name'],
-                                details=args['details'])
+                                project_name=args['project_name'],
+                                project_details=args['project_details'])
         if resp:
             return {'message': 'Project Created'}, 200
         else:
@@ -37,16 +37,16 @@ class Project(Resource):
 
     def put(self):
         parser = reqparse.RequestParser(trim=True, bundle_errors=True)
-        parser.add_argument('name', type=nonEmptyString, required=True, nullable=False)
-        parser.add_argument('details', type=str, required=True, nullable=False)
+        parser.add_argument('project_name', type=nonEmptyString, required=True, nullable=False)
+        parser.add_argument('project_details', type=str, required=True, nullable=False)
         args = parser.parse_args()
-        check = db.getProject(name=args['name'])
+        check = db.getProject(project_name=args['project_name'])
         if check is False:
             return {'message': 'Project does not exist'}, 400
-        if check['details'] == args['details']:
+        if check['project_details'] == args['project_details']:
             return {'message': 'Project details already updated'}, 400
-        done = db.updateProject(name=args['name'],
-                                details=args['details'])
+        done = db.updateProject(project_name=args['project_name'],
+                                project_details=args['project_details'])
         if done:
             return {'message': 'Project details is updated'}, 200
         else:
@@ -55,12 +55,12 @@ class Project(Resource):
       
     def delete(self):
         parser = reqparse.RequestParser(trim=True, bundle_errors=True)
-        parser.add_argument('name', type=nonEmptyString, required=True,nullable=False)
+        parser.add_argument('project_name', type=nonEmptyString, required=True,nullable=False)
         args = parser.parse_args()
-        check = db.getProject(name=args['name'])
+        check = db.getProject(project_name=args['project_name'])
         if check is False:
             return {'message': 'Project does not exist'}, 400
-        done = db.deleteProject(name=args['name'])
+        done = db.deleteProject(name=args['project_name'])
         if done:
             return {'message': 'Project is deleted'}, 200
         if not done:
