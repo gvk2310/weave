188a103c2cd600b86ebea3a49b836850b58413e6
Mishra, Sumit D <sumit.d.mishra@accenture.com>
2021-06-23 10:54:23 +0000

    jwt and secret key

--- UserManagement/UserMgmt/src/config/config.py
+++ UserManagement/UserMgmt/src/config/config.py
@@ -7,8 +7,8 @@ app.config['MONGODB_SETTINGS'] = {
     'connect': False
 }
 mongohost = os.environ['mongohost']
-#app.config['SECRET_KEY'] = os.environ['secretkey']
-# app.config['JWT_SECRET_KEY'] = os.environ['jwtsecretkey']
+app.config['SECRET_KEY'] = os.environ['secretkey']
+app.config['JWT_SECRET_KEY'] = os.environ['jwtsecretkey']
 mywd_iv = base64.b64decode(os.environ['MYWD_IV'])
 mywd_key = base64.b64decode(os.environ['MYWD_KEY'])
 service_user = os.environ['service_user']
