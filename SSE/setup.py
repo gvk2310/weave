cb3a2df888d77ebea559955a4fa94b32a3dff1d2
Mishra, Sumit D <sumit.d.mishra@accenture.com>
2021-06-28 11:55:56 +0000

    setup.py edited online with Bitbucket

--- SSE/setup.py
+++ SSE/setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 
 ext_modules = [
     Extension("sse.src.api.resources", ["sse/src/api/resources.py"]),
-    Extension("sse.src.lib.commonFunctions", ["sse/src/lib/commonFunctions"]),
+    Extension("sse.src.lib.commonFunctions", ["sse/src/lib/commonFunctions.py"]),
     Extension("sse.src.config.config", ["sse/src/config/config.py"]),
     Extension("sse.src.__init__", ["sse/src/__init__.py"]),
     Extension("sse.src.log", ["sse/src/log.py"])
