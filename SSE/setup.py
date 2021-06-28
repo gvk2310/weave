98606d55594afe85f1ea341f7169df8267227646
Mishra, Sumit D <sumit.d.mishra@accenture.com>
2021-06-28 11:45:59 +0000

    setup.py edited online with Bitbucket

--- SSE/setup.py
+++ SSE/setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+
 ext_modules = [
-        Extension("sse.src.api.resources",  ["sse/src/api/resources.py"]),
-                Extension("sse.src.config.config",  ["sse/src/config/config.py"]),
-  				Extension("sse.src.__init__",  ["sse/src/__init__.py"]),
-  				Extension("sse.src.log",  ["sse/src/log.py"]),
-  				Extension("sse.src.lib.commonFunctions",  ["sse.src.lib.commonFunctions"])
+    Extension("sse.src.api.resources", ["sse/src/api/resources.py"]),
+    Extension("sse.src.lib.commonFunctions", ["sse/src/lib/commonFunctions"]),
+    Extension("sse.src.config.config", ["sse/src/config/config.py"]),
+    Extension("sse.src.__init__", ["sse/src/__init__.py"]),
+    Extension("sse.src.log", ["sse/src/log.py"])
 ]
 setup(
-        name = 'devnetops',
-        cmdclass = {'build_ext': build_ext},
-        ext_modules = ext_modules
+    name='devnetops',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules
 )
+
