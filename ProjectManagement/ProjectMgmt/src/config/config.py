12e0f6b51488e29f908c21764143089c06e4ca59
Gummadi, Raagitha <raagitha.gummadi@accenture.com>
2022-03-17 12:29:24 +0000

    weavescope

--- ProjectManagement/ProjectMgmt/src/config/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from .. import app
-
-app.config['MONGODB_SETTINGS'] = {
-    'host': os.environ['mongohost'],
-    'connect': False
-}
-token_auth_url = os.environ['usermgmtUrl']
--- SSE/sse/src/config/config.py
+++
@@ -1,5 +0,0 @@
-import os
-from .. import app
-
-app.config['upload_folder'] = 'tmp'
-token_auth_url = os.environ['usermgmtUrl']
